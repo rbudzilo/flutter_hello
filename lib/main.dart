a6ca197a3adc03051642d767108edc0273989ed8
budzilo <roman@budzilo.ru>
2023-07-18 22:11:31 +0300

    По нажатию на кнопку, в консоли печатается "Консоль"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Text('Hello, World'),
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Text', style: TextStyle(
+            fontSize: 20,
+            color: Colors.red,
+            fontFamily: 'Times New Roman'
+          ),),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          backgroundColor: Colors.deepOrangeAccent,
+          onPressed: () {
+            print('Консоль');
+          },
+      )
+      ),
+    );
+  }
+}
